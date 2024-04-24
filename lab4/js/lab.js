0c60c294cbbe49a70a09defdc1bfd8605252ceaf
moreof <mdcarmic@gmail.com>
2024-04-23 20:13:38 -0700

    lab 4

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,6 +1,39 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// Lab 4: Pseudocoding and problem-solving
+// Author: Morris Carmichael
+// Date: 4/23/2024
+
+// Making sourdough starter
+// 1. Verify that you have the required materials: whole wheat flour, non-chlorinated water, a glass bowl, a resealable glass jar or similar container, a measuring cup, a cheesecloth, and a fork.
+// 2. Measure out one cup of whole wheat flour in your measuring cup.
+// 3. Add this flour to the glass bowl.
+// 4. Measure out three fourths of a cup of water in your measuring cup.
+// 5. Add this water to the glass bowl.
+// 6. Mix the flour and water together in the bowl using the fork until a dough is formed, and there is little to no loose flour visible.
+// 7. Loosely cover the bowl with the cheesecloth, and let sit approximately 24 hours in a warm spot (75-80 degrees Fahrenheit is ideal).
+// 8. After 24 hours have passed, remove the cheesecloth and mix the dough with the fork well.
+// 9. Measure out half a cup of this dough and discard it.
+// 10. Add half of a cup of flour to the remaining dough in the bowl.
+// 11. Add three eighths of a cup (or six tablespoons) of water to the remaining dough in the bowl.
+// 12. Mix the newly added flour and water into the dough with the fork until there is little to no loose flour visible.
+// 13. Repeat steps 7 through 12 until six days have passed.
+// 14. Check the strength and bubbliness of your dough mixture; if the mixture is not yet bubbling, begin feeding your mixture twice a day (performing steps 10-12 is “feeding”).
+// 15. Cover the bowl loosely with a cheesecloth, and wait another 24 hours.
+// 16. On day 7, your sourdough starter should be ready to use. Transfer your mixture to your resealable container.
+// 17. You now have sourdough starter. Whenever your sourdough starter is used, replenish the starter with an equivalent amount of flour and water – one cup of starter would be substituted with one cup of flour and three fourths of a cup of water, for reference.
+
+// Rock paper scissors
+// 1. Initiate game
+// 2. Randomize variables “rock”, “paper”, “scissors”
+// 3. Accept user input “rock”, “paper”, or “scissors”
+// 4. Select a randomized variable
+// 5. Output randomized variable
+// 6a. If user input = “rock”, then variable “scissors” = loss and variable “paper” = win
+// 6b. If user input = “paper”, then variable “scissors” = win and variable “rock” = lose
+// 6c. If user input = “scissors”, then variable “rock” = loss and variable “paper” = win
+// 7. Validate user input
+// 8. Display user input and selected variable
+// 9. isplay result screen showing player loss or win
+
 
 // Constants
 
