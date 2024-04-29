6816be973e782ea7dbd9ec8fad30e8acc58d1a29
moreof <mdcarmic@gmail.com>
2024-04-28 17:24:46 -0700

    lab 6 + some updates

--- lab6/js/lab.js
+++ lab6/js/lab.js
@@ -1,6 +1,28 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - Lab 6: Arrays and Objects
+// Author: Morris Carmichael
+// Date:4/27/2024
+
+//Variables
+myTransport = ["Bike", "Car", "Bus", "Walking"];
+
+//Object
+myMainRide = {
+make: "Toyota",
+model: "Camry",
+color: "black",
+year: "1992"
+}
+
+//Object Age Program
+currentYear = "2024"
+age = function() {
+  return year - currentYear
+}
+
+//Output
+document.writeln("How I Get Around:" + myTransport + "<br>" );
+document.writeln("My Main Ride: <pre>",
+    JSON.stringify(myMainRide, null, '\t'), "</pre>")
 
 // Constants
 
