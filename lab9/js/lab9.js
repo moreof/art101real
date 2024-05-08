343f9e60fd2be9bae4eb99fbfbbd09787d76b787
moreof <mdcarmic@gmail.com>
2024-05-07 19:33:37 -0700

    lab 9 progress + upkeep
    centered buttons throughout
    tried to fix lab 9 buttons

--- lab9/js/lab9.js
+++ lab9/js/lab9.js
@@ -7,11 +7,20 @@
 //Add Buttons: Using jQuery, append buttons to your challenge, problems, and results sections of your document.
 //add buttons to sections
 $("#challenge").append("<button id='button-challenge'>Make Special</button>");
-$("#problems").append("<button id='button-challenge'>Make Special</button>");
-$("#results").append("<button id='button-challenge'>Make Special</button>");
-$("#reflection").append("<button id='button-challenge'>Make Special</button>");
+$("#problems").append("<button id='button-problems'>Make Special</button>");
+$("#results").append("<button id='button-results'>Make Special</button>");
+$("#reflection").append("<button id='button-reflection'>Make Special</button>");
 //Add an Event Listener: Add an click listener to each button. Here's one:
 // add a click listener to the challenge button
 $("button-challenge").click(function(){
     $("#challenge").toggleClass("special");
+});
+$("button-problems").click(function(){
+    $("#problems").toggleClass("special");
+});
+$("button-results").click(function(){
+    $("#results").toggleClass("special");
+});
+$("button-reflection").click(function(){
+    $("#reflection").toggleClass("special");
 });
