1894cd01cb8edfebc51717fbf52a3f15e9bc722d
moreof <mdcarmic@gmail.com>
2024-05-07 18:05:59 -0700

    partial lab 9 + upkeep
    streamlined some code, added more consistent and readable styling throughout

---
+++ lab9/js/lab9.js
@@ -0,0 +1,17 @@
+/*
+   lab9.js - JavaScript/jQuery script to use buttons to modify elements on a page
+   Requirements: jQuery must be loaded for this script to work.
+   Author: Morris Carmichael
+   Date: 5/7/2024
+*/
+//Add Buttons: Using jQuery, append buttons to your challenge, problems, and results sections of your document.
+//add buttons to sections
+$("#challenge").append("<button id='button-challenge'>Make Special</button>");
+$("#problems").append("<button id='button-challenge'>Make Special</button>");
+$("#results").append("<button id='button-challenge'>Make Special</button>");
+$("#reflection").append("<button id='button-challenge'>Make Special</button>");
+//Add an Event Listener: Add an click listener to each button. Here's one:
+// add a click listener to the challenge button
+$("button-challenge").click(function(){
+    $("#challenge").toggleClass("special");
+});
