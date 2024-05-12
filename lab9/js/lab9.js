f75882ad2dfdfb818eea5ad28fba75a2e1165f79
moreof <mdcarmic@gmail.com>
2024-05-12 14:16:15 -0700

    lab9 working

--- lab9/js/lab9.js
+++ lab9/js/lab9.js
@@ -1,31 +1,27 @@
 /*
    lab9.js - JavaScript/jQuery script to use buttons to modify elements on a page
-   Requirements: jQuery must be loaded for this script to work.
    Author: Morris Carmichael
    Date: 5/7/2024
 */
-//Add Buttons: Using jQuery, append buttons to your challenge, problems, and results sections of your document.
 //add buttons to sections
 $("#challenge").append("<button id='button-challenge'>Make Special</button>");
 $("#problems").append("<button id='button-problems'>Make Special</button>");
 $("#results").append("<button id='button-results'>Make Special</button>");
 $("#reflection").append("<button id='button-reflection'>Make Special</button>");
-//Add an Event Listener: Add an click listener to each button. Here's one:
-
-// add a click listener to the challenge button
-$("button-challenge").click(function(){
-    $("#challenge").toggleClass("special");
+// add click listener to buttons and toggle classes
+$("#button-challenge").click(function(){
     $("#challenge").toggleClass("minor-section");
+    $("#challenge").toggleClass("special");
 });
-$("button-problems").click(function(){
-    $("#problems").toggleClass("special");
+$("#button-problems").click(function(){
     $("#problems").toggleClass("minor-section");
+    $("#problems").toggleClass("special");
 });
-$("button-reflection").click(function(){
-    $("#reflection").toggleClass("special");
+$("#button-reflection").click(function(){
     $("#reflection").toggleClass("minor-section");
+    $("#reflection").toggleClass("special");
 });
-$("button-results").click(function(){
-    $("#results").toggleClass("special");
+$("#button-results").click(function(){
     $("#results").toggleClass("minor-section");
+    $("#results").toggleClass("special");
 });
