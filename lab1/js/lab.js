0d0937786c7108bfc820fcffcd185c61720e3d31
moreof <mdcarmic@gmail.com>
2024-05-13 09:58:06 -0700

    lab 9 task x!

--- lab1/js/lab.js
+++ lab1/js/lab.js
@@ -19,3 +19,32 @@ function main() {
 
 // let's get this party started
 main();
+
+/*
+$(button-challenge.button).click(function(){
+    $(this).parent().toggleClass("minor-section");
+    $(this).parent().toggleClass("special");
+})
+*/
+
+//Hint 3: Within a jQuery callback the keyword $(this) refers to the thing being selected. 
+//The jQuery method .parent() selects the parent of the selected element. 
+//So within the callback, we can use $(this).parent() to select the section that the button is in!
+/*
+$("#button-challenge").click(function(){
+    $("#challenge").toggleClass("minor-section");
+    $("#challenge").toggleClass("special");
+});
+$("#button-problems").click(function(){
+    $("#problems").toggleClass("minor-section");
+    $("#problems").toggleClass("special");
+});
+$("#button-reflection").click(function(){
+    $("#reflection").toggleClass("minor-section");
+    $("#reflection").toggleClass("special");
+});
+$("#button-results").click(function(){
+    $("#results").toggleClass("minor-section");
+    $("#results").toggleClass("special");
+});
+*/
--- lab9/js/lab9.js
+++ lab9/js/lab9.js
@@ -4,24 +4,8 @@
    Date: 5/7/2024
 */
 //add buttons to sections
-$("#challenge").append("<button id='button-challenge'>Make Special</button>");
-$("#problems").append("<button id='button-problems'>Make Special</button>");
-$("#results").append("<button id='button-results'>Make Special</button>");
-$("#reflection").append("<button id='button-reflection'>Make Special</button>");
+$(".minor-section").append("<button class='button-challenge'>Make Special</button>");
 // add click listener to buttons and toggle classes
-$("#button-challenge").click(function(){
-    $("#challenge").toggleClass("minor-section");
-    $("#challenge").toggleClass("special");
-});
-$("#button-problems").click(function(){
-    $("#problems").toggleClass("minor-section");
-    $("#problems").toggleClass("special");
-});
-$("#button-reflection").click(function(){
-    $("#reflection").toggleClass("minor-section");
-    $("#reflection").toggleClass("special");
-});
-$("#button-results").click(function(){
-    $("#results").toggleClass("minor-section");
-    $("#results").toggleClass("special");
+$(".button-challenge").click(function(){
+    $(this).parent().toggleClass("minor-section special");
 });
